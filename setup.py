e5593c65447019426b3a82f2e13e7c9f5a3494be
rorry <rabrenne19@gmail.com>
2025-12-28 23:25:31 -0500

    new version for compile

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="perforatedai",
     # Remember to also edit setupCython
-    version="3.0.6",
+    version="3.0.7",
     packages=["perforatedai"],
     author="PerforatedAI",
     author_email="rorry@perforatedai.com",
