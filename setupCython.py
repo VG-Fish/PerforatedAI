4598602bdd4208be32859e21efaf64ac2b8a2e74
Rorry Brenner <47458848+RorryB@users.noreply.github.com>
2025-12-29 00:05:44 -0500

    Develop (#26)
    * Add package wheels for e3.0.6 (Python 3.9)
    
    * Add package wheels for e3.0.6 (Python 3.11)
    
    * Add package wheels for e3.0.6 (Python 3.10.15)
    
    * Add package wheels for e3.0.6 (Python 3.8)
    
    * Add package wheels for e3.0.6 (Python 3.6)
    
    * Add package wheels for e3.0.6 (Python 3.12)
    
    * Add package wheels for e3.0.6 (Python 3.7)
    
    * Add package manylinux wheels for e3.0.6
    
    * Add package wheels for e3.0.6 (Python 3.11)
    
    * Add package wheels for e3.0.6 (Python 3.12)
    
    * Add package wheels for e3.0.6 (Python 3.10.x)
    
    * Add package wheels for e3.0.6 (Python 3.13)
    
    * Add package wheels for e3.0.6 (Python 3.8)
    
    * Add package wheels for e3.0.6 (Python 3.9)
    
    * Add package wheels for e3.0.6 (Python 3.10.x)
    
    * Add package wheels for e3.0.6 (Python 3.11)
    
    * Add package wheels for e3.0.6 (Python 3.9)
    
    * Add package wheels for e3.0.6 (Python 3.8)
    
    * Add package wheels for e3.0.6 (Python 3.12)
    
    * Add package wheels for e3.0.6 (Python 3.7)
    
    * Add package wheels for e3.0.6 (Python 3.6)
    
    * Add package wheels for e3.0.6 (Python 3.9)
    
    * Add package wheels for e3.0.6 (Python 3.8)
    
    * Add package wheels for e3.0.6 (Python 3.10.15)
    
    * Add package wheels for e3.0.6 (Python 3.6)
    
    * Add package wheels for e3.0.6 (Python 3.11)
    
    * Add package wheels for e3.0.6 (Python 3.12)
    
    * Add package wheels for e3.0.6 (Python 3.7)
    
    * new version for compile
    
    * Add package wheels for e3.0.7 (Python 3.10.15)
    
    * Add package wheels for e3.0.7 (Python 3.8)
    
    * Add package wheels for e3.0.7 (Python 3.12)
    
    * Add package wheels for e3.0.7 (Python 3.9)
    
    * Add package wheels for e3.0.7 (Python 3.6)
    
    * Add package wheels for e3.0.7 (Python 3.11)
    
    * Add package wheels for e3.0.7 (Python 3.7)
    
    * Add package manylinux wheels for e3.0.7
    
    * Add package wheels for e3.0.7 (Python 3.12)
    
    * Add package wheels for e3.0.7 (Python 3.13)
    
    * Add package wheels for e3.0.7 (Python 3.10.x)
    
    * Add package wheels for e3.0.7 (Python 3.8)
    
    * Add package wheels for e3.0.7 (Python 3.9)
    
    * Add package wheels for e3.0.7 (Python 3.11)
    
    * Add package wheels for e3.0.7 (Python 3.9)
    
    * Add package wheels for e3.0.7 (Python 3.10.x)
    
    * Add package wheels for e3.0.7 (Python 3.11)
    
    * Add package wheels for e3.0.7 (Python 3.12)
    
    * Add package wheels for e3.0.7 (Python 3.7)
    
    * Add package wheels for e3.0.7 (Python 3.8)
    
    * Add package wheels for e3.0.7 (Python 3.6)
    
    * Add package wheels for e3.0.7 (Python 3.8)
    
    * Add package wheels for e3.0.7 (Python 3.10.15)
    
    * Add package wheels for e3.0.7 (Python 3.9)
    
    * Add package wheels for e3.0.7 (Python 3.11)
    
    * Add package wheels for e3.0.7 (Python 3.6)
    
    * Add package wheels for e3.0.7 (Python 3.12)
    
    * Add package wheels for e3.0.7 (Python 3.7)
    
    ---------
    
    Co-authored-by: PAI GitHub Actions <actions@perforatedai.dev.local>

--- setupCython.py
+++ setupCython.py
@@ -53,7 +53,7 @@ class build_py(_build_py):
 
 setup(
     name="perforatedai",
-    version="3.0.5",
+    version="3.0.7",
     packages=find_packages(),
     author="PerforatedAI",
     author_email="rorry@perforatedai.com",
