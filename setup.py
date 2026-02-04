fefd35766fb510c983f9bf54d218632c5e726222
rorry <rabrenne19@gmail.com>
2026-02-04 18:29:34 -0500

    updated setups

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="perforatedai",
     # Remember to also edit setupCython
-    version="3.0.8",
+    version="3.0.9",
     packages=["perforatedai"],
     author="PerforatedAI",
     author_email="rorry@perforatedai.com",
