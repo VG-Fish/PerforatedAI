9b478bd2ad22bf315198de8b17f3c67d8f497e52
Rorry <rabrenne19@gmail.com>
2026-07-18 07:24:16 -0700

    updated version

--- setupCython.py
+++ setupCython.py
@@ -59,7 +59,7 @@ else:
 
 setup(
     name="perforatedai",
-    version="3.2.2",
+    version="3.2.3",
     packages=find_packages(),
     author="PerforatedAI",
     author_email="rorry@perforatedai.com",
